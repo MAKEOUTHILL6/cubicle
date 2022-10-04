d15f703fd9ca5061419ac3c21421794eede5d71f
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-10-04 21:55:05 +0300

    Implemented search funcionality

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,5 +10,22 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.post('/search', (req, res) => {
+    let currentSearch = req.body;
+    let searchedCubeName = currentSearch.search;
+    let searchedCubeDiffFrom = currentSearch.from;
+    let searchedCubeDiffTo = currentSearch.to;
+    let cubesArray = req.cubes;
+
+    let searchedCubes = cubesArray
+    .filter(x => x.name.startsWith(searchedCubeName))
+    .filter(x => x.difficulty >= searchedCubeDiffFrom)
+    .filter(x => x.difficulty <= searchedCubeDiffTo);
+    
+    res.render('index', {
+        cubes: searchedCubes,
+    });
+});
+
 
 module.exports = router;
