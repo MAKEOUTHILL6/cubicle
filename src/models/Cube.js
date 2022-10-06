c63af8dfc9d32e02c8f23d3ad21f276424b41bea
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-10-06 11:23:03 +0300

    Cube schema added

---
+++ src/models/Cube.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const cubeSchema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        maxLength: 150,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true, 
+
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 10,
+    },
+});
+
+
+cubeSchema.path('imageUrl').validate(function() {
+    return this.imageUrl.startsWith('http' || 'https');
+}, `Image URL should start with https or http`);
+
+
+const Cube = mongoose.model("Cube", cubeSchema);
+
+
+exports.Cube = Cube;
