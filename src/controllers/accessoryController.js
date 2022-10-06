52fa07a45291c12da1fa30cd40751ca216706967
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-10-06 22:39:47 +0300

    Added dynamic acc rendering

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Accessory = require('../models/Accessory');
+const {Cube} = require('../models/Cube');
 
 router.get('/create-accessory', (req, res) => {
     res.render('createAccessory');
@@ -9,6 +10,17 @@ router.get('/create-accessory', (req, res) => {
 router.post('/create-accessory', async (req, res) => {
     await Accessory.create(req.body);
     res.redirect('/');
+});
+
+
+router.get('/attach/:id', async (req, res) => {
+    let cube = await Cube.findById(req.params.id).lean();
+    let accessories = await Accessory.find().lean();
+
+    res.render('attachAccessory', {
+        cube,
+        accessories,
+    });
 });
 
 
