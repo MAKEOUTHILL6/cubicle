8dbec8dc9c2b7311a2daf130e0a91e445af75f26
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-10-06 10:03:43 +0300

    Inital commit

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const homeController = require('./controllers/homeController');
 const createController = require('./controllers/createController');
+const accessoryController = require('./controllers/accessoryController');
 
 const router = express.Router();
 
 
 router.use('/', homeController);
 router.use('/cube', createController);
+router.use('/accessory', accessoryController);
 
 module.exports = router;
